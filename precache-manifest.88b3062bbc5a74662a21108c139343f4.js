922a5efb6c778072825a878961ba3af7af5ce8b2
Pedro Martins (OSV) <pedro.martins.osv@fedex.com>
2019-09-04 12:00:40 -0500

    Updates

---
+++ precache-manifest.88b3062bbc5a74662a21108c139343f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56446b6f2f75b80c491b",
+    "url": "/rmaslite/static/js/runtime~main.56446b6f.js"
+  },
+  {
+    "revision": "346cf196aa3dc311f885",
+    "url": "/rmaslite/static/js/main.346cf196.chunk.js"
+  },
+  {
+    "revision": "fc7022e448dc089ee158",
+    "url": "/rmaslite/static/js/2.fc7022e4.chunk.js"
+  },
+  {
+    "revision": "346cf196aa3dc311f885",
+    "url": "/rmaslite/static/css/main.5c643827.chunk.css"
+  },
+  {
+    "revision": "fc7022e448dc089ee158",
+    "url": "/rmaslite/static/css/2.4ff04ab8.chunk.css"
+  },
+  {
+    "revision": "a3b5b31b71b9e97db650581d8ba127ad",
+    "url": "/rmaslite/index.html"
+  }
+];
